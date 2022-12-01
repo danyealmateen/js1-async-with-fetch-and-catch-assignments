bc8fa70a34299621a03dc9ec19d5c84b0c474ac8
danyealmateen <danyeal.mateen@hotmail.com>
2022-12-01 13:25:57 +0100

    asd

--- script.js
+++ script.js
@@ -298,42 +298,173 @@
 //1. Låt en användare söka på en typ av aktivitet. Hämta en random aktivitet och visa i DOM:en
 //http://www.boredapi.com/api/activity?type=recreational
 
+// let button = document.getElementById("button");
+// let activeDiv = document.createElement("div");
+
+// button.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   clearContainer();
+//   getActivity();
+// });
+
+// function getActivity() {
+//   let input = document.getElementById("input").value;
+//   let inputNumber = document.getElementById("inputNumber").value;
+//   let checkbox = document.getElementById("checkbox");
+//   const FREE = 0.0;
+
+//   const price = checkbox.checked ? `&price=${FREE}` : "";
+
+//   let url = `http://www.boredapi.com/api/activity?type=${input}&participants=${inputNumber}${price}`;
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       let activePara = document.createElement("p");
+//       activePara.innerHTML = `${data.activity}`;
+//       activeDiv.appendChild(activePara);
+//       document.body.appendChild(activeDiv);
+//       console.log(data);
+//     })
+//     .catch((error) => {
+//       let errorMessage = document.createElement("div");
+//       errorMessage.innerHTML = `ERROR ERROR --> ${error}`;
+//       document.body.appendChild(errorMessage);
+//       console.log(error);
+//     });
+// }
+
+// function clearContainer() {
+//   activeDiv.innerHTML = "";
+// }
+
+//REMAKE DOING IT ALL OVER AGAIN!
+// DOG API
+//1. Hämta 10 random bilder av en hundras som användaren får ange. Visa alla bilder på webbsidan.
+//2. Låt även användaren bestämma hur många bilder som ska hämtas. Tex 5 husky, 2 dalmatians osv.
+
+// let button = document.getElementById("button");
+// let createDiv = document.createElement("div");
+// let errorMessage = document.createElement("p");
+
+// button.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   clearContainer();
+//   tenPicturesOfDogs();
+//   console.log("click works");
+// });
+
+// function tenPicturesOfDogs() {
+//   let input = document.getElementById("input").value;
+//   let inputNum = document.getElementById("inputNum").value;
+//   let url = `https://dog.ceo/api/breed/${input}/images/random/${inputNum}`;
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       data.message.forEach((pic) => {
+//         createDiv.innerHTML += `<img src="${pic}"/>`;
+//       });
+//       document.body.appendChild(createDiv);
+//       console.log(data.message);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       errorMessage.innerHTML = `ERROR ERROR --> ${error}`;
+//       document.body.appendChild(errorMessage);
+//     });
+// }
+
+// function clearContainer() {
+//   createDiv.innerHTML = "";
+// }
+
+//CHUCK NORRIS JOKES!
+//1.Låt användaren söka på en kategori och hämta ett random skämt i den kategorin. Visa skämtet.
+
+// let button = document.getElementById("button");
+// let createDiv = document.createElement("div");
+// button.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   generateJoke();
+// });
+
+// function generateJoke() {
+//   let input = document.getElementById("input").value;
+//   let url = `https://api.chucknorris.io/jokes/random?category=${input}`;
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       createDiv.innerHTML = `${data.value}`;
+//       document.body.appendChild(createDiv);
+//       console.log(data);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// }
+
+//PUNK API
+//1. Låt en användare söka på öl genom att ange ett namn. Visa namn, bild och beskrivning för ett enda öl.
+// let input = document.getElementById("input").value;
+// let button = document.getElementById("button");
+// let createDiv = document.createElement("div");
+// let createPara = document.createElement("p");
+
+// button.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   getBeer();
+// });
+
+// function getBeer() {
+//   let input = document.getElementById("input").value;
+//   let url = `https://api.punkapi.com/v2/beers?beer_name=${input}`;
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       data.forEach((obj) => {
+//         createPara.innerHTML = `${obj.name} <hr> ${obj.description}<hr> <img src="${obj.image_url}"/> `;
+//       });
+//       createDiv.appendChild(createPara);
+//       document.body.appendChild(createDiv);
+//       console.log(data);
+//     })
+//     .catch((error) => {
+//       let errorMessage = document.createElement("p");
+//       errorMessage.innerHTML = ` ERROR ERROR ==> ${error}`;
+//       document.body.appendChild(errorMessage);
+//       console.log(error);
+//     });
+// }
+
+//2.Låt en användare ange vilken mat som ska passa till ölen och sök fram öl som passar. Visa namnet för alla öl i svaret.
+
 let button = document.getElementById("button");
-let activeDiv = document.createElement("div");
+let createDiv = document.createElement("div");
+let createPara = document.createElement("p");
 
 button.addEventListener("click", (event) => {
   event.preventDefault();
-  clearContainer();
-  getActivity();
+  checkBeerFood();
 });
 
-function getActivity() {
+function checkBeerFood() {
   let input = document.getElementById("input").value;
-  let inputNumber = document.getElementById("inputNumber").value;
-  let checkbox = document.getElementById("checkbox");
-  const FREE = 0.0;
-
-  const price = checkbox.checked ? `&price=${FREE}` : "";
-
-  let url = `http://www.boredapi.com/api/activity?type=${input}&participants=${inputNumber}${price}`;
-
+  let url = `https://api.punkapi.com/v2/beers?food=${input}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      let activePara = document.createElement("p");
-      activePara.innerHTML = `${data.activity}`;
-      activeDiv.appendChild(activePara);
-      document.body.appendChild(activeDiv);
+      data.forEach((obj) => {
+        createPara.innerHTML += ` <li>${obj.name}</li>`;
+      });
+      createDiv.appendChild(createPara);
+      document.body.appendChild(createDiv);
       console.log(data);
     })
     .catch((error) => {
-      let errorMessage = document.createElement("div");
-      errorMessage.innerHTML = `ERROR ERROR --> ${error}`;
+      let errorMessage = document.createElement("h1");
+      errorMessage.innerHTML = `ERROR ERROR ==> ${error}`;
       document.body.appendChild(errorMessage);
       console.log(error);
     });
-}
-
-function clearContainer() {
-  activeDiv.innerHTML = "";
 }
